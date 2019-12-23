82ca28b525e573c9d2c43433d38a8ef3a34749f3
Reha Sterbin <reha@omniti.com>
2019-12-23 16:38:50 -0500

    Add hikes route and template

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var hikeRouter = require('./routes/hike');
 
 var app = express();
 
@@ -21,6 +22,9 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('/hikes', hikeRouter.index);
+app.post('/add_hike', hikeRouter.add_hike);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
