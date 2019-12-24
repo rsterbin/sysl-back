5f40e8434436b285f699588e372a8d2192f50d0d
Reha Sterbin <reha@omniti.com>
2019-12-24 06:44:38 -0500

    Get this working locally as well

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var hikeRouter = require('./routes/hike');
 
 var app = express();
 
@@ -18,13 +18,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-app.get('/hikes', hikeRouter.index);
-app.post('/add_hike', hikeRouter.add_hike);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* POST users create */
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  console.log(process.env.RDS_HOSTNAME); // TODO: let app switch between RDS and local postgres
+  res.send('post to create a user');
+});
+
 module.exports = router;
