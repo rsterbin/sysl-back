42eeba3439e9dae337f8f4d0656445767379e471
Reha Sterbin <reha@omniti.com>
2020-01-05 19:46:23 -0500

    Small upgrade + todo list

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,13 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   console.log(req.body);
   console.log(process.env.RDS_HOSTNAME); // TODO: let app switch between RDS and local postgres
+
+  // TODO: Create a db user with appropriate permissions for localhost:sysl
+  // TODO: Using hardcoded connection info, connect to sysl and select pronouns (the only table that currently has data)
+  // TODO: Figure out where config data should be stored in EB/Express
+  // TODO: Set up switching between use of localhost and RDS according to the current environment
+  // TODO: Set up this endpoint to create a user
+
   res.send('post to create a user');
 });
 
