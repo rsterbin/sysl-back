a5d081fab1fd710caac554e2ee50c2de57c5a604
Reha Sterbin <reha@omniti.com>
2020-01-06 15:58:15 -0500

    Basic db connection

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var { pgAdapter } = require('../lib/pgAdapter.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+    const dbh = pgAdapter();
+    const queryText = 'SELECT * from pronouns';
+    dbh.query(queryText)
+        .then((response) => {
+            console.log(response);
+            dbh.end();
+            var buffer = '<ul>';
+            for (var i = 0; i < response.rows.length; ++i) {
+                var row = response.rows[i];
+                var num = i + 1;
+                buffer += '<li>Row #' + num + '<ul>';
+                for (var k in row) {
+                    buffer += '<li>' + k + ': ' + row[k] + '</li>';
+                }
+                buffer += '</ul></li>';
+            }
+            res.send(buffer);
+        })
+        .catch((err) => {
+            console.log(err);
+            dbh.end();
+            res.send('There was an error: ' + err);
+        });
+
 });
 
 /* POST users create */
 router.post('/', function(req, res, next) {
-  console.log(req.body);
-  console.log(process.env.RDS_HOSTNAME); // TODO: let app switch between RDS and local postgres
-
-  // TODO: Create a db user with appropriate permissions for localhost:sysl
-  // TODO: Using hardcoded connection info, connect to sysl and select pronouns (the only table that currently has data)
-  // TODO: Figure out where config data should be stored in EB/Express
-  // TODO: Set up switching between use of localhost and RDS according to the current environment
+    console.log(req.body);
   // TODO: Set up this endpoint to create a user
 
   res.send('post to create a user');
